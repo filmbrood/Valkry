2b61a133925d1cec4dd6873bc00979521ecd17f0
filmbrood <filmbrood@gmail.com>
2020-05-26 17:41:16 -0500

    Updated logging with timestamps

--- Valkry-Sandbox/src/sandbox.cpp
+++ Valkry-Sandbox/src/sandbox.cpp
@@ -6,6 +6,9 @@ class sandbox : public valkry::app
 public:
 	void Run()
 	{
+		valkry::LogWarn("Warning Test");
+		valkry::LogError("Error Test");
+		valkry::LogFatal("Fatal Test");
 		while (true);
 	}
 };
--- Valkry/src/logging.cpp
+++ Valkry/src/logging.cpp
@@ -1,11 +1,18 @@
 #include "logging.h"
 #include <iostream>
+#include <chrono>
+#include <ctime>
 
 namespace valkry {
 
 	void LogInfo(std::string desc, double data)
 	{
-		std::cout << "[time-placeholder] [VALKRY-INFO] " << desc;
+		std::chrono::system_clock::time_point systemTime = std::chrono::system_clock::now();
+		std::time_t epochSeconds = std::chrono::system_clock::to_time_t(systemTime);
+
+		auto* timeOutput = std::localtime(&epochSeconds);
+
+		std::cout << "[" << timeOutput->tm_hour << ":" << timeOutput->tm_min << ":" << timeOutput->tm_sec << "] [VALKRY-INFO] " << desc;
 		
 		if (data)
 			std::cout << " - " << data;
@@ -15,7 +22,12 @@ namespace valkry {
 
 	void LogWarn(std::string desc, double data)
 	{
-		std::cout << "[time-placeholder] [VALKRY-WARN] " << desc;
+		std::chrono::system_clock::time_point systemTime = std::chrono::system_clock::now();
+		std::time_t epochSeconds = std::chrono::system_clock::to_time_t(systemTime);
+
+		auto* timeOutput = std::localtime(&epochSeconds);
+
+		std::cout << "[" << timeOutput->tm_hour << ":" << timeOutput->tm_min << ":" << timeOutput->tm_sec << "] [VALKRY-INFO] " << desc;
 
 		if (data)
 			std::cout << " - " << data;
@@ -25,7 +37,12 @@ namespace valkry {
 
 	void LogError(std::string desc, double data)
 	{
-		std::cout << "[time-placeholder] [VALKRY-ERROR] " << desc;
+		std::chrono::system_clock::time_point systemTime = std::chrono::system_clock::now();
+		std::time_t epochSeconds = std::chrono::system_clock::to_time_t(systemTime);
+
+		auto* timeOutput = std::localtime(&epochSeconds);
+
+		std::cout << "[" << timeOutput->tm_hour << ":" << timeOutput->tm_min << ":" << timeOutput->tm_sec << "] [VALKRY-ERROR] " << desc;
 
 		if (data)
 			std::cout << " - " << data;
@@ -35,7 +52,12 @@ namespace valkry {
 
 	void LogFatal(std::string desc, double data)
 	{
-		std::cout << "[time-placeholder] [VALKRY-FATAL] " << desc;
+		std::chrono::system_clock::time_point systemTime = std::chrono::system_clock::now();
+		std::time_t epochSeconds = std::chrono::system_clock::to_time_t(systemTime);
+
+		auto* timeOutput = std::localtime(&epochSeconds);
+
+		std::cout << "[" << timeOutput->tm_hour << ":" << timeOutput->tm_min << ":" << timeOutput->tm_sec << "] [VALKRY-FATAL] " << desc;
 
 		if (data)
 			std::cout << " - " << data;
