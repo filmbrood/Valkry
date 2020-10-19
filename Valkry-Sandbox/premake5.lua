5777a947151c36542332055096777254cc29a1a1
Dawn Moore <63686840+filmbrood@users.noreply.github.com>
2020-10-19 06:41:24 -0500

    Update premake5.lua

--- Valkry-Sandbox/premake5.lua
+++ Valkry-Sandbox/premake5.lua
@@ -36,7 +36,7 @@ project "Valkry-Sandbox"
 		optimize "On"
 
 	configuration { "linux", "gmake2" }
-	  linkoptions {"-pthread -ldl" }
+	  linkoptions {"CC=gcc CXX=g++ -pthread -ldl" }
 
 	configuration { "windows", "gmake2" }
 		linkoptions { "-lglfw3 -lgdi32 -lgdiplus" }
